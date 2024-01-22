6f7923ab274ecbb40817f625a04acadf4f7593f0
Ryan Tucker <ryan.tucker@mymail.champlain.edu>
2024-01-22 17:38:20 -0500

    added base movment and jumping, working on making it snappier :3

--- Desert Soul/Assets/Scripts/PlayerMovement.cs
+++ Desert Soul/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float horizontal;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpPower = 16f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,35 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f) 
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
     }
 }
