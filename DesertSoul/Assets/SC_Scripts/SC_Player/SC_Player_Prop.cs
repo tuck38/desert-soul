b579fc94c27c56e92f608dec46a5e33533bd131b
Caren Tucker <ryantucker212@gmail.com>
2025-10-09 17:39:28 -0400

    got the initial set up done, should be easy from here

--- DesertSoul/Assets/SC_Scripts/SC_Player/SC_Player_Prop.cs
+++ DesertSoul/Assets/SC_Scripts/SC_Player/SC_Player_Prop.cs
@@ -7,17 +7,30 @@ public class SC_Player_Prop : MonoBehaviour
     [SerializeField] private int maxHealth;
     [SerializeField] private int currentHealth;
 
+    //Sun Beam Variables, putting here for now
+    [SerializeField] private int maxSunStacks;
+    [SerializeField] private int sunStacksBeforeDamage;
+    private int currentSunStacks = 0;
+    private bool inSunlight = false;
+
+    [SerializeField] private float sunStackRemovalFrequency = 0.2f;
+    private float sunStackTimer;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        sunStackTimer = sunStackRemovalFrequency;
         GameManager.Instance.newScene();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!inSunlight && currentSunStacks > 0)
+        {
+            depleteSunStacks();
+        }
     }
     
     public void TakeDamage(int dmg)
@@ -32,6 +45,30 @@ public class SC_Player_Prop : MonoBehaviour
             //TODO: Good for now but change this to restarting from save
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             
+        }
+    }
+
+    public void giveSunStack()
+    {
+        inSunlight = true;
+        currentSunStacks++;
+    }
+
+    public void OutOfSun()
+    {
+        inSunlight = false;
+    }
+
+    private void depleteSunStacks()
+    {
+        if (sunStackTimer > 0)
+        {
+            sunStackTimer -= Time.deltaTime;
+            if (sunStackTimer <= 0)
+            {
+                currentSunStacks--;
+                sunStackTimer = sunStackRemovalFrequency;
+            }
         }
     }
 }
