53c7b8c059a53431c584d232833d02a5f5965c81
ryan tucka <ryantucker212@gmail.com>
2026-01-17 12:21:36 -0500

    started UI work

---
+++ DesertSoul/Assets/SC_Scripts/HUD/SC_Player_HUD.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Player_HUD : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> Health;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateHealthUI(int currentHealth, int maxHealth)
+    {
+        
+    }
+}
