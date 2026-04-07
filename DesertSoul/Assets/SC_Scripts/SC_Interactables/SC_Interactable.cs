1deec742bd29041deaa6027a4bfd74497964017b
ryan tucka <ryantucker212@gmail.com>
2026-04-06 21:23:56 -0400

    made a script for the interiors of buildings, added both of them just need to decorate :3

---
+++ DesertSoul/Assets/SC_Scripts/SC_Interactables/SC_Interactable.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+public class SC_Interactable : MonoBehaviour
+{
+    [SerializeField] string goToScene;
+
+    [SerializeField] float buttonHoldTime = 2f;
+    private float currentButtonHoldTime = 0f;
+
+    [SerializeField] bool needsHold = false;
+
+    [SerializeField] GameObject interactButton;
+
+    private SC_Player_Move player;
+
+    private bool playerInRange = false;
+
+    private bool holding = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HoldInputs();
+        if(holding)
+        {
+            HoldTimer();
+        }
+    }
+
+    void HoldInputs()
+    {
+        if(playerInRange)
+        {
+
+            holding = player.IsInteracting();
+
+            if(holding == false)
+            {
+                currentButtonHoldTime = 0f;
+            }
+        }
+    }
+
+    void HoldTimer()
+    {
+        if(currentButtonHoldTime < buttonHoldTime)
+        {
+            currentButtonHoldTime += Time.deltaTime;
+        }
+        else
+        {
+            GoToScene();
+        }
+    }
+
+
+    void GoToScene()
+    {
+        GameManager.Instance.LoadNewLevel(goToScene, SC_Enum_Doors.Bottom, false, true);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            player = collision.GetComponent<SC_Player_Move>();
+            playerInRange = true;
+            interactButton.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            playerInRange = false;
+            interactButton.SetActive(false);
+            holding = false;
+            currentButtonHoldTime = 0f;
+        }
+    }
+}
