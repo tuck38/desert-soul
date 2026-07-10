51d56e6f1de8422d284532b7e6cb68161b29f5d7
Ilan Fiarman <imf4565@gmail.com>
2026-07-10 13:38:56 -0400

    credits screen made and programmed, need to add names

--- DesertSoul/Assets/SC_Scripts/SC_Menus/SC_Menus_Start.cs
+++ DesertSoul/Assets/SC_Scripts/SC_Menus/SC_Menus_Start.cs
@@ -1,16 +1,25 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class SC_Menus_Start : MonoBehaviour
 {
 
     [SerializeField] string LastScene;
+    [SerializeField] GameObject creditsPanel;
+    [SerializeField] RectTransform creditTransform;
+    [SerializeField] GameObject mainScreen;
+    public float credSpd;
     //[Header("Wwise Events")]
     //public AK.Wwise.Event mainMenuButtons;
-    void Start()
+    bool activeCredits = false;
+    void Update()
     {
-        
+        if (activeCredits)
+        {
+            creditTransform.anchoredPosition += new Vector2(0, +credSpd * Time.deltaTime);
+        }
     }
 
     public void StartGame ()
@@ -24,9 +33,34 @@ public class SC_Menus_Start : MonoBehaviour
         //mainMenuButtons.Post(gameObject);
     }
 
+    public void Credits()
+    {
+        creditsPanel.SetActive(true);
+        StartCoroutine(rollCreds());
+        
+    }
+
+    public void Back()
+    {
+        creditsPanel.SetActive(false);
+        activeCredits = false;
+        //optinsMenu.SetActive(false);
+        mainScreen.SetActive(true);
+    }
+
     public void QuitGame()
     {
         //mainMenuButtons.Post(gameObject);
         Application.Quit();
+    }
+
+    IEnumerator rollCreds()
+    {
+        yield return new WaitForSeconds(3f);
+        
+        activeCredits = true;
+
+        yield return null;
+        
     }
 }
