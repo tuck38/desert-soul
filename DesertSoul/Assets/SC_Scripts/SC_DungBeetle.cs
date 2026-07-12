15697e5ebb55ebaccc9e5cb3c6dfb31258a77210
Ilan Fiarman <imf4565@gmail.com>
2026-07-12 10:06:07 -0400

    starting ball thrown

--- DesertSoul/Assets/SC_Scripts/SC_DungBeetle.cs
+++ DesertSoul/Assets/SC_Scripts/SC_DungBeetle.cs
@@ -5,27 +5,195 @@ public class SC_DungBeetle : MonoBehaviour
 {
     [SerializeField] int MAXhp;
     [SerializeField] Transform createPoint;
-    int currHP;
     bool ballMade = false;
     [SerializeField] private Animator animator;
     [SerializeField] GameObject dungBall;
     SC_DungBall ballCode;
 
-    void Start()
+    [SerializeField] protected float MAXHealth;
+    protected float currentHealth;
+    [SerializeField] private int damage;
+    
+    //kb vars
+    [SerializeField] private float knockbackDist;
+    [SerializeField] private float kbLERPTime;
+
+    private Transform lockPoint;
+
+    private Rigidbody2D rb;
+
+    protected bool locked;
+
+    private float lockTimer = 0.5f;
+    private float currentLockTimer = 0f;
+    protected bool lockCooldown;
+
+    //Launch Vars
+    private bool launchFromRight = true;
+    private float launchTime = 0;
+    [SerializeField] private float totalLaunchTime;
+    [SerializeField] private float launchPower;
+
+    [SerializeField] private float decelerationMult = 0.95f;
+    private bool decelerationEnabled = false;
+
+    //REFERENCE GOTTEN AFTER PLAYER HITS ENEMY
+    private GameObject player;
+
+    //spawner stuff, temp, use events later
+    SC_WaveRoom roomSpawned;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected virtual void Start()
     {
-        currHP = MAXhp;
+        currentHealth = MAXHealth;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    // Update is called once per frame
+    protected virtual void Update()
     {
-        if (currHP <= 0)
+
+        if (currentHealth <= 0)
         {
-            Destroy(this);
+            Die();
+        }
+
+        if(currentLockTimer > 0)
+        {
+            currentLockTimer -= Time.deltaTime;
+            if (currentLockTimer <= 0)
+            {
+                lockCooldown = false;
+            }
+        }
+
+        if (locked)
+        {
+            gameObject.transform.position = new Vector3(lockPoint.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
+
+        //drill knockback timer 
+        if(launchTime > 0)
+        {         
+            //This system works for now, but doesent use gravity and feels floaty
+            if (launchFromRight)
+            {
+                rb.linearVelocity = new Vector2(-launchPower, launchPower / 2);
+            }
+            else
+            {
+                rb.linearVelocity = new Vector2(launchPower, launchPower / 2);
+            }
+
+            launchTime -= Time.deltaTime;
+            if(launchTime <= 0)
+            {
+               decelerationEnabled = true;
+            }
+        }
+
+        if (decelerationEnabled)
+        {
+            decelerate();
         }
 
         if (!ballMade){
             StartCoroutine(createBall());
             ballMade = true;
+        }
+    }
+
+    //takes damage and returns true if the attack killed the enemy
+    public virtual bool TakeDamage(SC_Attack_Base attack, Transform carryPoint)
+    {
+        if (!locked)
+        {
+            currentHealth -= attack.getDamage();
+            if (currentHealth <= 0)
+            {
+                return true;
+            }
+
+            if (attack.shouldCarry() && !lockCooldown)
+            {
+                locked = true;
+                lockPoint = carryPoint;
+            }
+        }
+        return false;
+    }
+
+    public void setMommaSpawner(SC_WaveRoom lockRoom)
+    {
+        roomSpawned = lockRoom;
+    }
+
+    public void Knockback(AttackType atkType)
+    {
+
+        //method 1
+        //Vector3 dirVect = gameObject.transform.position - enemy.transform.position;
+        //dirVect.Normalize();
+        //rb.AddForce(dirVect * launchPower, ForceMode2D.Impulse);
+        //rb.linearVelocity = dirVect * launchPower;
+
+        //method 2
+        if(atkType == AttackType.drillSide)
+        {
+            launchTime = totalLaunchTime;
+        }
+
+        Vector3 target = transform.position;
+
+        if (player.transform.position.x >= transform.position.x)
+        {
+            launchFromRight = true;
+            target = new Vector3(transform.position.x - knockbackDist, transform.position.y, transform.position.z);
+        }
+        else if (player.transform.position.x < transform.position.x)
+        {
+            launchFromRight = false;
+            target = new Vector3(transform.position.x + knockbackDist, transform.position.y, transform.position.z);
+        }
+
+        if(atkType == AttackType.primary)
+        {
+            //ima make this a lerp
+            transform.position = target;
+        }
+    }
+
+    public int GetDamage()
+    { 
+        return damage; 
+    }
+
+    public void SetPlayer(GameObject player)
+    {
+        this.player = player;
+    }
+    
+    public void setLocked(bool shouldLock)
+    {
+        locked = shouldLock;
+        if (!locked)
+        {
+            lockCooldown = true;
+            currentLockTimer = lockTimer;
+            Knockback(AttackType.drillSide);
+        }
+    }
+
+    private void decelerate()
+    {
+        rb.linearVelocity = new Vector2 (rb.linearVelocityX * decelerationMult, rb.linearVelocityY);
+
+        if (rb.linearVelocity.x <= 0.2 && rb.linearVelocity.x >= -0.2)
+        {
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+            decelerationEnabled = false;
         }
     }
 
@@ -35,5 +203,14 @@ public class SC_DungBeetle : MonoBehaviour
         yield return new WaitForSeconds(2f);
         ballMade = false;
         
+    }
+
+    private void Die()
+    {
+        if(roomSpawned != null)
+        {
+            roomSpawned.checkWave();
+        }
+        Destroy(gameObject);
     }
 }
