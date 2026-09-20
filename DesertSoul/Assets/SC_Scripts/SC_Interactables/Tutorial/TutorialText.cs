e2b773944889d9891d4b418dc4dff5982d591d6d
Ilan Fiarman <imf4565@gmail.com>
2026-09-20 19:33:36 -0400

    draft 1 of tutorial done

--- DesertSoul/Assets/SC_Scripts/SC_Interactables/Tutorial/TutorialText.cs
+++ DesertSoul/Assets/SC_Scripts/SC_Interactables/Tutorial/TutorialText.cs
@@ -3,8 +3,19 @@ using UnityEngine;
 public class TutorialText : MonoBehaviour
 {
     public GameObject tutText;
-    /*private OnTriggerEnter2D(Collider2D col)
+    void Start()
     {
-        
-    }*/
+        tutText.SetActive(false);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        tutText.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        tutText.SetActive(false);
+        Destroy(this);
+    }
 }
